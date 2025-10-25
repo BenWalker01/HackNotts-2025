5395d0e8d0e3b7087a60a0252b25886b28bb46cc
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-10-25 16:57:03 +0100

    Add Character assets (#2)
    * initial assets
    
    * character walks better

--- game.py
+++ game.py
@@ -3,31 +3,33 @@ import sys
 from map import Map
 from player import Player
 
+
 class Game:
-    def __init__(self,player, map):
-        self.player : Player = player
-        self.map : Map = map
-        
+    def __init__(self, player, map):
+        self.player: Player = player
+        self.map: Map = map
+
     def run(self):
+        clock = pygame.time.Clock()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                
-                keys = pygame.key.get_pressed()
-                
-                if keys[pygame.K_a]:
-                    self.player.move_left()
-                if keys[pygame.K_d]:
-                    self.player.move_right()
-                if keys[pygame.K_w]:
-                    self.player.move_up()
-                if keys[pygame.K_s]:
-                    self.player.move_down()
-            
+
+            keys = pygame.key.get_pressed()
+            self.player.walking = False
+            if keys[pygame.K_a]:
+                self.player.move_left()
+            elif keys[pygame.K_d]:
+                self.player.move_right()
+            elif keys[pygame.K_w]:
+                self.player.move_up()
+            elif keys[pygame.K_s]:
+                self.player.move_down()
+
             self.map.update_screen()
             self.player.draw()
-            pygame.display.update()
-            
+            pygame.display.flip()
+            clock.tick(60)
--- player.py
+++ player.py
@@ -1,21 +1,72 @@
 import pygame
+import os
 
-SPEED = 5
+SPEED = 2
+
 
 class Player:
-    def __init__(self,window):
-        self.x,self.y = (100,100)
+    def __init__(self, window):
+        self.x, self.y = (100, 100)
         self.window = window
-        
-        
+
+        self.frames_walk = self._load_sheets()
+        self.walking = False
+        self.direction = "u"
+        self.animation_frame = 0
+        self.animation_speed = 0.15
+        self.current_frame = self.frames_walk[self.direction][0]
+
+    def _load_sheets(self):
+        frames_folder_root = "assets/character/models/sequences/leather_armor"
+        frames_folder_walk = os.path.join(frames_folder_root, "walk")
+        frame_files = sorted(f for f in os.listdir(frames_folder_walk))
+        frames_per_direction = {
+            "l": [],
+            "r": [],
+            "u": [],
+            "d": []
+        }
+        order = ["u", "l", "d", "r"]
+        for filename in frame_files:
+            img = pygame.image.load(os.path.join(
+                frames_folder_walk, filename)).convert_alpha()
+            width = img.get_width()
+            height = img.get_height() // 4
+
+            for i in range(4):
+                frame = img.subsurface(pygame.Rect(
+                    0, i * height, width, height))
+                frames_per_direction[order[i]].append(frame)
+        return frames_per_direction
+
+    def animate(self):
+        self.animation_frame += self.animation_speed
+        self.animation_frame = self.animation_frame % len(
+            self.frames_walk[self.direction])
+        self.current_frame = self.frames_walk[self.direction][int(
+            self.animation_frame)]
+
     def draw(self):
-        pygame.draw.circle(self.window.screen, (0,0,255), (self.x,self.y), 5)
-        
+        if self.walking:
+            self.animate()
+        self.window.screen.blit(self.current_frame, (self.x, self.y))
+
     def move_up(self):
+        self.direction = "u"
         self.y -= SPEED
+        self.walking = True
+
     def move_down(self):
+        self.direction = "d"
+        self.walking = True
         self.y += SPEED
+
     def move_left(self):
+        self.direction = "l"
         self.x -= SPEED
+        self.walking = True
+
     def move_right(self):
+        self.direction = "r"
         self.x += SPEED
+        self.walking = True
